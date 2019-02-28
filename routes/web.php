c7d10b87dbc5217cd741aa8d6f9e5b209397b558
winter <88@ex.com>
2019-02-28 07:18:52 +0000

    init

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('about','StaticPagesController@about')->name('about');
+Route::get('help','StaticPagesController@help')->name('help');
+
+Route::get('signup','UsersController@signup')->name('signup');
+Route::resource('users','UsersController');
+
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
+
